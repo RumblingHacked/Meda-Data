ba04372313f4bf3d52759d55ebd619abc1be4c21
RumblingHacked <rumblinghacked@gmail.com>
2019-04-22 18:40:28 -0500

    Removed Files

--- app/core/constant/constants.ts
+++
@@ -1,7 +0,0 @@
-export const FIREBASE_CONFIG = {
-    apiKey: "AIzaSyAxEOoP6jPV6N5Wiqa3dNhqF23_Atpxe88",
-    authDomain: "medadata-854d0.firebaseapp.com",
-    databaseURL: "https://medadata-854d0.firebaseio.com",
-    storageBucket: "medadata-854d0.appspot.com",
-    messagingSenderId: "225718314818"
-  };
